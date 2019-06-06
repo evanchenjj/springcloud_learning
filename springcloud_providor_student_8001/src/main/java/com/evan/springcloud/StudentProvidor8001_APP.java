c4848712d00a7621fc8873f5603f47f48448ffa2
evanchenjj <evanchenjj@gmail.com>
2019-06-06 21:23:39 +0800

    针对注册的调整优化

--- springcloud_providor_student_8001/src/main/java/com/evan/springcloud/StudentProvidor8001_APP.java
+++ springcloud_providor_student_8001/src/main/java/com/evan/springcloud/StudentProvidor8001_APP.java
@@ -2,7 +2,7 @@ package com.evan.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Created by IntelliJ IDEA
@@ -12,7 +12,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @description
  */
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class StudentProvidor8001_APP {
 
 	public static void main(String[] args) {
